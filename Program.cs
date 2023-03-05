7c1fc4e7891ff8f4212287b07a0ef0a77d8f92ef
jerrita <je5r1ta@icloud.com>
2023-03-05 13:54:03 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.InteropServices;
+using NAudio.Midi;
+
+namespace GenshinMidiBinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            var keyMap = new Dictionary<int, char>
+            {
+                { 60, 'A' },
+                { 62, 'S' },
+                { 64, 'D' },
+                { 65, 'J' },
+                { 67, 'K' },
+                { 69, 'L' },
+                { 57, ' ' }
+            };
+
+            var midiIn = new MidiIn(0);
+            midiIn.MessageReceived += (sender, e) =>
+            {
+                var commandCode = e.MidiEvent.CommandCode;
+                if (commandCode == MidiCommandCode.NoteOn || commandCode == MidiCommandCode.NoteOff)
+                {
+                    var note = ((NoteEvent)e.MidiEvent).NoteNumber;
+                    if (keyMap.ContainsKey(note))
+                    {
+                        var key = keyMap[note];
+                        if (e.MidiEvent.CommandCode == MidiCommandCode.NoteOn)
+                        {
+                            GameInput.KeyDown(key);
+                        }
+                        else if (e.MidiEvent.CommandCode == MidiCommandCode.NoteOff)
+                        {
+                            GameInput.KeyUp(key);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown key: " + note.ToString());
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Not a NodeEvent");
+                }
+            };
+
+            midiIn.Start();
+            Console.WriteLine("Listener launched. Press any key to exit.");
+            Console.ReadKey();
+            midiIn.Stop();
+        }
+    }
+
+    static class GameInput
+    {
+        public static void KeyDown(char key)
+        {
+            keybd_event((byte)key, 0, 0, (UIntPtr)0);
+        }
+
+        public static void KeyUp(char key)
+        {
+            keybd_event((byte)key, 0, 2, (UIntPtr)0);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# GenshinMidiBinder
+
+BOME MIDI Player 替代品，但是很简陋
+
+- 这是一个 Midi 键盘转键盘输入的小程序，可以用来弹琴（x
+- 目前只映射了 C1 - C6 到 `ASDJKL`，可以用来打曲子
+- 如果你有其他需求，可以在代码里添加映射，如果可以的话希望您能提一个 PR
+- 不知道这种实现会不会导致封号，可能换用 DirectX 的 `SendInput` 会更稳妥一些
+
+## 实现方式
+
+- https://github.com/naudio/NAudio
+- https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
